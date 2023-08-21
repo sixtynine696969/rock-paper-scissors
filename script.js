e161a332b0329eeb0db8f9cfb94d5e302d27dd52
sixtynine696969 <134623948+sixtynine696969@users.noreply.github.com>
2023-08-21 13:57:54 +0200

    Lint and prettify code

--- script.js
+++ script.js
@@ -1,77 +1,83 @@
 function getComputerChoice() {
-    return ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
+  return ['Rock', 'Paper', 'Scissors'][Math.floor(Math.random() * 3)];
 }
 
 function capitalizeString(string) {
-    return string[0].toUpperCase() + string.slice(1);
+  return string[0].toUpperCase() + string.slice(1);
 }
 
 function playRound(playerSelection, computerSelection) {
-    let lcPlayerSelection = playerSelection.trim().toLowerCase();
-    let lcComputerSelection = computerSelection.trim().toLowerCase()
+  const lcPlayerSelection = playerSelection.trim().toLowerCase();
+  const lcComputerSelection = computerSelection.trim().toLowerCase();
 
-    let capPlayerSelection = capitalizeString(lcPlayerSelection);
-    let capComputerSelection = capitalizeString(lcComputerSelection);
+  const capPlayerSelection = capitalizeString(lcPlayerSelection);
+  const capComputerSelection = capitalizeString(lcComputerSelection);
 
-    switch(true) {
-        case lcPlayerSelection === "rock" && lcComputerSelection === 'scissors':
-        case lcPlayerSelection === 'scissors' && lcComputerSelection === 'paper':
-        case lcPlayerSelection === 'paper' && lcComputerSelection === 'rock':
-            return `You Win! ${capPlayerSelection} ${('scissors' === lcPlayerSelection) ? 'beat' : 'beats'} ${capComputerSelection}.`;
-        case lcPlayerSelection === "scissors" && lcComputerSelection === 'rock':
-        case lcPlayerSelection === 'paper' && lcComputerSelection === 'scissors':
-        case lcPlayerSelection === 'rock' && lcComputerSelection === 'paper':
-            return `You Lose! ${capComputerSelection} ${('scissors' === lcComputerSelection) ? 'beat' : 'beats'} ${capPlayerSelection}.`;
-        default:
-            return `Tie! ${capPlayerSelection} ${('scissors' === lcPlayerSelection) ? "don't" : "doesn't"} beat ${capComputerSelection}.`;
-    }
-}
-
-function checkInputValidity(string) {
-    return ['rock', 'paper', 'scissors'].includes(string.toLowerCase());
+  switch (true) {
+    case lcPlayerSelection === 'rock' && lcComputerSelection === 'scissors':
+    case lcPlayerSelection === 'scissors' && lcComputerSelection === 'paper':
+    case lcPlayerSelection === 'paper' && lcComputerSelection === 'rock':
+      return `You Win! ${capPlayerSelection} ${
+        lcPlayerSelection === 'scissors' ? 'beat' : 'beats'
+      } ${capComputerSelection}.`;
+    case lcPlayerSelection === 'scissors' && lcComputerSelection === 'rock':
+    case lcPlayerSelection === 'paper' && lcComputerSelection === 'scissors':
+    case lcPlayerSelection === 'rock' && lcComputerSelection === 'paper':
+      return `You Lose! ${capComputerSelection} ${
+        lcComputerSelection === 'scissors' ? 'beat' : 'beats'
+      } ${capPlayerSelection}.`;
+    default:
+      return `Tie! ${capPlayerSelection} ${
+        lcPlayerSelection === 'scissors' ? "don't" : "doesn't"
+      } beat ${capComputerSelection}.`;
+  }
 }
 
 function game() {
+  let playerScore = 0;
+  let computerScore = 0;
 
-    let playerScore = 0;
-    let computerScore = 0;
-    
-    const buttons = document.querySelectorAll('button');
-    const score = document.querySelector('.score');
-    const outputDiv = document.querySelector('.output')
+  const buttons = document.querySelectorAll('button');
+  const score = document.querySelector('.score');
+  const outputDiv = document.querySelector('.output');
 
-    buttons.forEach(button => {
-        button.addEventListener('click', (button) => {
-            const selectedElementID = button.target.id;
-            const output = playRound(selectedElementID, getComputerChoice())
+  buttons.forEach((button) => {
+    button.addEventListener('click', (button) => {
+      const selectedElementID = button.target.id;
+      const output = playRound(selectedElementID, getComputerChoice());
 
-            switch (true) {
-                case output.startsWith('You Win!'):
-                    ++playerScore;
-                    break;
-                case output.startsWith('You Lose!'):
-                    ++computerScore;
-                    break;
-            }
+      switch (true) {
+        case output.startsWith('You Win!'):
+          playerScore += 1;
+          break;
+        case output.startsWith('You Lose!'):
+          computerScore += 1;
+          break;
+        default:
+          break;
+      }
 
-            if (playerScore == 5) {
-                score.textContent = "You Won!";
-                score.style.backgroundColor = "rgb(167, 255, 164)";
+      if (playerScore === 5) {
+        score.textContent = 'You Won!';
+        score.style.backgroundColor = 'rgb(167, 255, 164)';
 
-                outputDiv.textContent = output;
-                playerScore = computerScore = 0;
-            } else if (computerScore == 5) {
-                score.textContent = "You lost!";
-                score.style.backgroundColor = "rgb(255, 148, 148)";
+        outputDiv.textContent = output;
+        playerScore = 0;
+        computerScore = 0;
+      } else if (computerScore === 5) {
+        score.textContent = 'You lost!';
+        score.style.backgroundColor = 'rgb(255, 148, 148)';
 
-                outputDiv.textContent = output;
-                playerScore = computerScore = 0;
-            } else {
-                score.textContent = `${playerScore} : ${computerScore}`;
-                outputDiv.textContent = output;
-                score.style.backgroundColor = "";
-            }
-        })
-    })
+        outputDiv.textContent = output;
+        playerScore = 0;
+        computerScore = 0;
+      } else {
+        score.textContent = `${playerScore} : ${computerScore}`;
+        outputDiv.textContent = output;
+        score.style.backgroundColor = '';
+      }
+    });
+  });
 }
-game()
+
+game();
